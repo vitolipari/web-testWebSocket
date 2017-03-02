bf3ff6d5ba28f61aa0f1791498a1b6a3852560a6
LipariStudios - Linux <vitolipari81@gmail.com>
2017-03-02 21:33:24 +0100

    inizio

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+define('DB_TYPE',       'mysql');
+define('DB_HOST',       'localhost');
+define('DB_NAME',       'test_queue_dev');
+define('DB_USER',       'vitolipari');
+define('DB_PASSWORD',   'wedra_db_dev');
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+
+/*
+
+- Fa n chiamate Ajax al server
+- il server risponde dopo 0.001 sec
+- il server una volta data la response, avvia l'accodatore
+- l'accodatore inserisce nella coda un dato
+- l'accodatore lancia lo stackEmptier
+- se lo stackEmptier non è in esecuzione viene avviato 
+- lo stackEmptier, prende il dato più vecchio dalla tabella della coda
+- lo stackEmptier impiega 2sec a svolgere un compito
+- viene svuotata la coda
+- viene notificata la fine dell'operazione se e presente un canale di notifica
+
+*/
+
+
+?>
+
+
+<!doctype>
+<html>
+
+	<head>
+		<title></title>
+		<script type="text/javascript" src="">
+			window.addEventListener('load', init, false);
+
+			/**
+			*/
+			function init(){}
+
+			/**
+			*/
+			function play(){}
+
+		</script>
+	</head>
+	<body>
+
+		<div id="" class="">
+			<div id="" class="">
+			</div>
+		</div>
+
+
+
+		<script type="text/javascript" src="../../framework/liparistudios/PHP/AllPack.php?lib=base"></script>
+	</body>
+</html>
