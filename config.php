ac6713c2d26e4e70d9a772083c8d0e9183bffa68
Mac <vitolipari81@gmail.com>
2017-04-05 23:19:09 +0200

    [Mac|PhpStorm] ho sistemato il file di configurazione che aveva i dati per il database

--- config.php
+++ config.php
@@ -2,12 +2,6 @@
 
 // dev
 
-define('DB_TYPE',       'mysql');
-define('DB_HOST',       'localhost');
-define('DB_NAME',       'test_queue_dev');
-define('DB_USER',       'vitolipari');
-define('DB_PASSWORD',   'wedra_db_dev');
-
 
 
 ?>
